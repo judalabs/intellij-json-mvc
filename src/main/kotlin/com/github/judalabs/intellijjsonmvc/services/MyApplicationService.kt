0fdcad3e9bb2e7a557bdd5179b06c9a9227762fe
GitHub Action <action@github.com>
2022-09-24 23:42:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/judalabs/intellijjsonmvc/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/judalabs/intellijjsonmvc/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.judalabs.intellijjsonmvc.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.judalabs.intellijjsonmvc.MyBundle
 
 class MyApplicationService {
 
